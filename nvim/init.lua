c48537242ab12c0f0295ac23a1932b9d7044688f
Julia Paluch <julia@greynoise.io>
2026-01-05 13:37:20 -0500

    feat: add format on save for .go files

--- nvim/init.lua
+++ nvim/init.lua
@@ -95,6 +95,25 @@ vim.lsp.config('clangd', {
 })
 vim.lsp.enable('clangd')
 
+vim.lsp.config('gopls', {
+	capabilities = capabilities,
+})
+vim.lsp.enable('gopls')
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+vim.api.nvim_create_autocmd(
+	"BufWritePost",
+	{
+		pattern = "*.go",
+		group = "AutoFormat",
+		callback = function()
+			vim.cmd("silent !gofmt -w %")
+			vim.cmd("edit")
+		end,
+	}
+)
+
 require("nvim-treesitter").setup({
   ensure_installed = { "cpp", "lua" }
 })
